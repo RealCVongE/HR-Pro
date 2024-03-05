e4cfcd3f8164a671d0016f083769e5c6c176dbec
yeowon <dudnjsckrgo@gmail.com>
2024-03-06 01:46:01 +0900

    bug fix

---
+++ run_beoid.sh
@@ -0,0 +1,4 @@
+python main.py --cfg beoid --stage 1 --mode train
+python main.py --cfg beoid --stage 1 --mode test
+python main.py --cfg beoid --stage 2 --mode train
+python main.py --cfg beoid --stage 2 --mode test
